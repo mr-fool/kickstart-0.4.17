1dc88f69b6a16bb5aa811e41947ac0f9012349e6
mr-fool <mr-fool@users.noreply.github.com>
2019-03-21 22:48:31 -0600

    forgot the ''

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './buildCampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     Json.parse(CampaignFactory.interface),
-    0x133092496ae191aDA0D68bf8ffa4f4FA55C053DB
+    '0x133092496ae191aDA0D68bf8ffa4f4FA55C053DB'
 
 );
 export default instance;
