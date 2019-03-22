b6e6165999caec27a1f2bbed26cf54147a43d724
mr-fool <mr-fool@users.noreply.github.com>
2019-03-21 21:00:26 -0600

    renamed newcampaign to index

---
+++ .next/static/development/pages/newcampaign.js
@@ -0,0 +1,86 @@
+((window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static\\development\\pages\\newcampaign.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fnewcampaign&absolutePagePath=C%3A%5CUsers%5Chetzer%5CDocuments%5CGitHub%5Ckickstart-0.4.17%5Cpages%5Cnewcampaign.js!./":
+/*!**************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fnewcampaign&absolutePagePath=C%3A%5CUsers%5Chetzer%5CDocuments%5CGitHub%5Ckickstart-0.4.17%5Cpages%5Cnewcampaign.js ***!
+  \**************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/newcampaign", function() {
+      var page = __webpack_require__(/*! ./pages/newcampaign.js */ "./pages/newcampaign.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/newcampaign.js */ "./pages/newcampaign.js", function() {
+          if(!next.router.components["/newcampaign"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/newcampaign.js */ "./pages/newcampaign.js")
+          next.router.update("/newcampaign", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_55dc4e2ecf7824085104 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_55dc4e2ecf7824085104 */ "dll-reference dll_55dc4e2ecf7824085104"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/newcampaign.js":
+/*!******************************!*\
+  !*** ./pages/newcampaign.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "C:\\Users\\hetzer\\Documents\\GitHub\\kickstart-0.4.17\\pages\\newcampaign.js";
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, "This is the new campaign page!!!");
+});
+
+/***/ }),
+
+/***/ 3:
+/*!******************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fnewcampaign&absolutePagePath=C%3A%5CUsers%5Chetzer%5CDocuments%5CGitHub%5Ckickstart-0.4.17%5Cpages%5Cnewcampaign.js ***!
+  \******************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fnewcampaign&absolutePagePath=C%3A%5CUsers%5Chetzer%5CDocuments%5CGitHub%5Ckickstart-0.4.17%5Cpages%5Cnewcampaign.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fnewcampaign&absolutePagePath=C%3A%5CUsers%5Chetzer%5CDocuments%5CGitHub%5Ckickstart-0.4.17%5Cpages%5Cnewcampaign.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_55dc4e2ecf7824085104":
+/*!*******************************************!*\
+  !*** external "dll_55dc4e2ecf7824085104" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_55dc4e2ecf7824085104;
+
+/***/ })
+
+},[[3,"static/runtime/webpack.js"]]]));;
+//# sourceMappingURL=newcampaign.js.map
